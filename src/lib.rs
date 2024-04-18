cdc2718a1052ef528af9ba36ef741565531a1993
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-04-18 10:51:10 +0200

    triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,35 @@
-pub struct Triangle;
+pub struct Triangle {
+    sides: [u64; 3],
+}
 
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        unimplemented!("Construct new Triangle from following sides: {sides:?}. Return None if the sides are invalid.");
+        // Check if sides form a valid triangle
+        if sides.iter().any(|&x| x == 0) ||
+            sides[0] + sides[1] <= sides[2] ||
+            sides[1] + sides[2] <= sides[0] ||
+            sides[0] + sides[2] <= sides[1] {
+           return None;
+        }
+
+        Some(Triangle { sides })
     }
 
     pub fn is_equilateral(&self) -> bool {
-        unimplemented!("Determine if the Triangle is equilateral.");
+        self.sides[0] == self.sides[1] && self.sides[1] == self.sides[2]
     }
 
     pub fn is_scalene(&self) -> bool {
-        unimplemented!("Determine if the Triangle is scalene.");
+        self.sides[0] != self.sides[1] && self.sides[1] != self.sides[2] && self.sides[0] != self.sides[2]
     }
 
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!("Determine if the Triangle is isosceles.");
+        self.sides[0] == self.sides[1] || self.sides[1] == self.sides[2] || self.sides[0] == self.sides[2]
+    }
+
+    pub fn is_degenerate(&self) -> bool {
+        self.sides[0] + self.sides[1] == self.sides[2] ||
+        self.sides[1] + self.sides[2] == self.sides[0] ||
+        self.sides[0] + self.sides[2] == self.sides[1]
     }
 }
